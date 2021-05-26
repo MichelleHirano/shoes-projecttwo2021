f6e29bd67a3a8aeb73c4c3d8858dc63db0b29cd7
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-05-26 04:46:52 -0700

    fixed logo not showing up on certain pages

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('images'));
+app.use(express.static('views/images/'));
 
 app.use(require('./controllers/'));
 
